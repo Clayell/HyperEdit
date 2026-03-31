c1b92edc03ea79bbe39c68844ee0a88a373e850b
Clayel <videogamer0907@gmail.com>
2026-03-31 17:30:56 -0400

    remove unnecessary log method
    principiawrapper.cs is just copied from TIP so i left some stuff in woops

--- Source/PrincipiaWrapper.cs
+++ Source/PrincipiaWrapper.cs
@@ -6,7 +6,7 @@ namespace HyperEdit
     {
         public static bool Init()
         {
-            Log("Attempting to find Principia....");
+            Extensions.Log("Attempting to find Principia....");
 
             foreach (AssemblyLoader.LoadedAssembly assembly in AssemblyLoader.loadedAssemblies)
             {
@@ -14,21 +14,19 @@ namespace HyperEdit
                 {
                     if (assembly.assembly.GetName().Name == "principia.ksp_plugin_adapter")
                     {
-                        Log($"Principia found.");
+                        Extensions.Log($"Principia found.");
                         return true;
                     }
                 }
                 catch (Exception ex)
                 {
-                    Log($"Error loading Principia: {ex}");
+                    Extensions.Log($"Error loading Principia: {ex}");
                 }
             }
 
-            Log($"Principia not found.");
+            Extensions.Log($"Principia not found.");
 
             return false;
         }
-
-        private static void Log(string message) => Extensions.Log(message);
     }
 }
